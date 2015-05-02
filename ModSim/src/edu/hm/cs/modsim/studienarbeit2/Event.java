3cc470d56661aeba8422485d4e454bfff7f7b516
tobibi100 <tobias.hoefer@outlook.de>
2015-05-02 19:41:31 +0200

    ABC-Event wurde erstellt

--- ModSim/src/edu/hm/cs/modsim/studienarbeit2/Event.java
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/Event.java
@@ -1,5 +1,11 @@
 package edu.hm.cs.modsim.studienarbeit2;
 
-public class Event {
+//ABC fuer alle events
+abstract class Event {
+
+  abstract int eventTime(); // Schnittstelle fuer die Zeiten des jeweiligen
+                            // events
+
+  abstract void processEvent();// veraenderung des Systemzustands
 
 }
