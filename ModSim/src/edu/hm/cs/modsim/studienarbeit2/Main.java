deb74d2cc885d880f72ed691b347b14be07eb2bf
tobibi100 <tobias.hoefer@outlook.de>
2015-05-02 14:03:10 +0200

    clean up

--- ModSim/src/edu/hm/cs/modsim/studienarbeit2/Main.java
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/Main.java
@@ -3,18 +3,13 @@ package edu.hm.cs.modsim.studienarbeit2;
 public class Main {
 
   public static void main(String[] args) {
-		//Kommentar
-	  //tobi war hier
-	  //Hubert
-    //endtest
-	  //neuer Kommentar
-	  //comment
-		Client cl = new Client();
-		cl.printClientMessage();
-		
-		Queue<Client> myQueue = new Queue<>();
-		myQueue.pushFront(cl);
-		myQueue.front().printClientMessage();
-		
-	}
+
+    Client cl = new Client();
+    cl.printClientMessage();
+
+    Queue<Client> myQueue = new Queue<>();
+    myQueue.pushFront(cl);
+    myQueue.front().printClientMessage();
+
+  }
 }
