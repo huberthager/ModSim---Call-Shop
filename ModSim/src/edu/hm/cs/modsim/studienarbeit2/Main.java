b1086641cfaef2dbba32574c78759f447d294922
tobibi100 <tobias.hoefer@outlook.de>
2015-05-02 19:33:10 +0200

    event driven simulation

--- ModSim/src/edu/hm/cs/modsim/studienarbeit2/Main.java
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/Main.java
@@ -3,15 +3,9 @@ package edu.hm.cs.modsim.studienarbeit2;
 public class Main {
 
   public static void main(String[] args) {
-
-    Client cl = new Client();
-
-    Queue queue = new Queue(2);
-    System.out.println(queue.size());
-    queue.addClient(cl);
-    queue.addClient(new Client());
-    System.out.println(queue.addClient(new Client()));
-    System.out.println(queue.size());
+    // FE erstellen
+    // scheduler erstellen
+    // run() methode ausfuehren
 
   }
 }
---
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/Scheduler.java
@@ -0,0 +1,23 @@
+package edu.hm.cs.modsim.studienarbeit2;
+
+public class Scheduler {
+  public int simTime;
+  public int endSimTime;
+
+  public Scheduler(int endSimTime) {
+    this.simTime = 0;
+    this.endSimTime = endSimTime;
+
+  }
+
+  /* Methode die die eig simulaton durchfuehrt */
+  public void run() {
+    // while(EventList.size()!=0 & simTime<endSimTime)
+    // hole dir das event mit der kleinesten Zeit
+    // erhoehe die Zeit simeTime um die eventTime
+    // fuehre das event aus
+    // loesche das event aus der eventListe
+
+  }
+
+}
