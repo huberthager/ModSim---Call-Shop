4fcef962871092fae4ce4652c649d8927b369020
tobibi100 <tobias.hoefer@outlook.de>
2015-05-08 17:24:38 +0200

    organisierte imports

--- ModSim/src/edu/hm/cs/modsim/studienarbeit2/EventList.java
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/EventList.java
@@ -1,43 +1,42 @@
 package edu.hm.cs.modsim.studienarbeit2;
 
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class EventList {
 
-	private List<Event> eventList;
+  private List<Event> eventList;
 
-	public EventList() {
-		eventList = new LinkedList<>();
-	}
+  public EventList() {
+    eventList = new LinkedList<>();
+  }
 
-	public void addEvent(Event e) {
-		this.eventList.add(e);
-	}
+  public void addEvent(Event e) {
+    this.eventList.add(e);
+  }
 
-	public void removeFirst() {
-		this.eventList.remove(0);
-	}
+  public void removeFirst() {
+    this.eventList.remove(0);
+  }
 
-	public void sortEventList() {
-		Collections.sort(eventList, new EventComparator());
-	}
+  public void sortEventList() {
+    Collections.sort(eventList, new EventComparator());
+  }
 
-	public boolean isEmpty() {
-		return eventList.size() == 0;
-	}
-	
-	public Event getFirst() {
-		return eventList.get(0);
-	}
-	
-	public String toString() {
-		String out = "";
-		for (Event e : eventList) {
-			out += " " + e.toString();
-		}
-		return out;
-	}
+  public boolean isEmpty() {
+    return eventList.size() == 0;
+  }
+
+  public Event getFirst() {
+    return eventList.get(0);
+  }
+
+  public String toString() {
+    String out = "";
+    for (Event e : eventList) {
+      out += " " + e.toString();
+    }
+    return out;
+  }
 }
