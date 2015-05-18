240f702fbb7c934897104e46d49b9e487ecbec6b
abstractCalculus <julian.bauer@outlook.com>
2015-05-18 12:49:59 +0200

    this is the new version

--- ModSim/src/edu/hm/cs/modsim/studienarbeit2/Main.java
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/Main.java
@@ -1,11 +1,21 @@
 package edu.hm.cs.modsim.studienarbeit2;
 
+import java.io.IOException;
+
 public class Main {
 
   public static void main(String[] args) {
     //SimSetup mit Parameter : Simulationsdauer , maximale Warteschlangenl�nge
-	  Scheduler s = new Scheduler(10,100);
+	  Scheduler s = new Scheduler(12,100);
 	  s.run();
+	  OutputWriter ow = new OutputWriter("Pfad");//Pfad spezifizieren
+	  try {
+		ow.writeOutput("Schreibe Output in FIle".toString());
+		ow.writeOutput("Ich schreibe erneut in mein OutputFile");
+	} catch (IOException e) {
+		// 
+		e.printStackTrace();
+	}
 	  
 	  
 
--- ModSim/src/edu/hm/cs/modsim/studienarbeit2/OutputWriter.java
+++ ModSim/src/edu/hm/cs/modsim/studienarbeit2/OutputWriter.java
@@ -1,20 +1,30 @@
 package edu.hm.cs.modsim.studienarbeit2;
 
 import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
 public class OutputWriter {
+	//TODO: InterArrivalTimes und ServingTimes in seperate Files schreiben
+	private OutputStream out2;
 	
-	private String content = "";
-	private byte[] bytes = content.getBytes();
+	public OutputWriter(String location) {
+		try {
+		out2 = new BufferedOutputStream(new FileOutputStream("/home/user1301/Documents/output2"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 	
-
 	public void writeOutput(String content) throws IOException {
-		try (OutputStream out = new BufferedOutputStream(new FileOutputStream("/home/user1301/Documents"))) {
+		byte[] bytes = content.getBytes();
+		try (OutputStream out = new BufferedOutputStream(new FileOutputStream("/home/user1301/Documents/output"))) {
             out.write(bytes);
             out.flush();
+            out2.write(bytes);
+            out2.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
